0bc3745f1e2c39fe20ecf009e5eea4638badb344
Mikko Haimila <haimila.mikko@gmail.com>
2020-11-13 02:00:10 +0200

    query for new training session and new exercise

--- frontend/src/queries.js
+++ frontend/src/queries.js
@@ -2,11 +2,34 @@ import { gql } from '@apollo/client'
 
 export const ALL_EXERCISES = gql`
 query {
-    allExercises {
-        movement
-        sets
-        reps
-        weight
+  allExercises {
+    movement
+    sets
+    reps
+    weight
+  }
+}
+`
+
+export const NEW_TRAINING_SESSION = gql`
+  mutation newTrainingSession($date: Int, $exercises: [Exercise]) {
+    addTrainingSession (
+      date: $date
+      exercises: $exercises
+    ){
+      id
+    }
+  }
+`
+export const NEW_EXERCISE = gql`
+  mutation newExercise($movement: String, $sets: Int, $reps: Int, $weight: Int) {
+    addExercise (
+      movement: $movement
+      sets: $sets
+      reps: $reps
+      weight: $weight
+    ){
+      id
     }
   }
 `
