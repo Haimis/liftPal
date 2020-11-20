745bfeff17868bf97896290bcbc4d203e3232457
Mikko Haimila <haimila.mikko@gmail.com>
2020-11-20 17:10:39 +0200

    typo fix

--- backend/models/trainingSession.js
+++ backend/models/trainingSession.js
@@ -4,10 +4,10 @@ const schema = new mongoose.Schema({
     date: {
         type: Date
     },
-    exercises: {
-        type: [mongoose.Schema.Types.ObjectId],
+    exercises: [{
+        type: mongoose.Schema.Types.ObjectId,
         ref: 'Exercise'
-    }
+    }]
 })
 
 module.exports = mongoose.model('TrainingSession', schema)
